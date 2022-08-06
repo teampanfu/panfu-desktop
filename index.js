2ef99e386f696c4675f4fca116ea632d225a4d6f
Robin <robin@panfu.us>
2022-08-06 18:36:09 +0200

    Refactoring

--- index.js
+++ index.js
@@ -1,89 +1,88 @@
-const { app, BrowserWindow, Menu } = require('electron')
+const { app, BrowserWindow } = require('electron')
 const { autoUpdater } = require('electron-updater')
 const path = require('path')
 
-// Allow users to only have one instance of the application
+const menu = require('./menu')
+
+let mainWindow
+
 const gotTheLock = app.requestSingleInstanceLock()
 
 if (!gotTheLock) {
     app.quit()
 } else {
-    // Specify the path to the Flash Player plugin
+    // Initialize the Flash Player plugin.
+    initFlashPlayer()
+
+    // Create the window, load the rest of the app, etc.
+    app.whenReady().then(() => {
+        createWindow()
+        autoUpdater.checkForUpdates()
+    })
+
+    // Create a window when none are open. (macOS)
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+
+    // Someone tried to run a second instance, we should focus our window.
+    app.on('second-instance', () => {
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore()
+            mainWindow.focus()
+        }
+    })
+
+    // Listen for the context menu event for each created window.
+    app.on('browser-window-created', (event, window) => {
+        window.webContents.on('context-menu', (event, params) => {
+            menu.popup(window, params.x, params.y)
+        })
+    })
+
+    // Quit when all windows are closed, except on macOS. There, it's common
+    // for applications and their menu bar to stay active until the user quits
+    // explicitly with Cmd + Q.
+    app.on('window-all-closed', function () {
+        if (process.platform !== 'darwin') app.quit()
+    })
+}
+
+function initFlashPlayer() {
     let pluginName
-    let resourcesPath = app.isPackaged ? process.resourcesPath : __dirname
 
     switch (process.platform) {
         case 'win32':
-            pluginName = app.isPackaged ? 'pepflashplayer.dll' : 'win/x64/pepflashplayer.dll'
+            pluginName = (app.isPackaged) ? 'pepflashplayer.dll' : 'win/x64/pepflashplayer.dll'
             break
         case 'darwin':
             pluginName = 'PepperFlashPlayer.plugin'
             break
-        case 'freebsd':
-        case 'linux':
-        case 'netbsd':
-        case 'openbsd':
+        default:
             pluginName = 'libpepflashplayer.so'
-
-            // Disable Chromium's sandbox
-            app.commandLine.appendSwitch('no-sandbox')
     }
+
+    // Disable Chromium's sandbox on Linux.
+    if (['freebsd', 'linux', 'netbsd', 'openbsd'].includes(process.platform)) {
+        app.commandLine.appendSwitch('no-sandbox')
+    }
+
+    const resourcesPath = (app.isPackaged) ? process.resourcesPath : __dirname
 
     app.commandLine.appendSwitch('ppapi-flash-path', path.join(resourcesPath, 'plugins', pluginName))
+    app.commandLine.appendSwitch('ppapi-flash-version', '32.0.0.465')
+}
 
-    // Create the browser window
-    let window = null
-
-    function createWindow() {
-        window = new BrowserWindow({
-            width: 1056,
-            height: 720,
-            show: false,
-            autoHideMenuBar: true,
-            webPreferences: {
-                contextIsolation: true,
-                plugins: true
-            }
-        })
-        window.once('ready-to-show', () => window.show())
-        window.loadURL('https://www.panfu.us/play')
-    }
-
-    // Context menu
-    const menu = Menu.buildFromTemplate([
-        { role: 'reload' }, { type: 'separator' },
-        { role: 'resetZoom' }, { role: 'zoomIn' }, { role: 'zoomOut' }
-    ])
-
-    app.on('browser-window-created', (event, win) => {
-        win.webContents.on('context-menu', (event, params) => {
-            menu.popup(win, params.x, params.y)
-        })
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1056,
+        height: 720,
+        autoHideMenuBar: true,
+        webPreferences: {
+            contextIsolation: true,
+            plugins: true
+        }
     })
 
-    // Set everything up as soon as our application is ready
-    app.whenReady().then(() => {
-        createWindow()
-
-        // Open a window if none are open (macOS)
-        app.on('activate', () => {
-            if (BrowserWindow.getAllWindows().length === 0) createWindow()
-        })
-
-        // Someone tried to run a second instance, we should focus our window
-        app.on('second-instance', () => {
-            if (window) {
-                if (window.isMinimized()) window.restore()
-                window.focus()
-            }
-        })
-
-        // Quit the app when all windows are closed
-        app.on('window-all-closed', () => {
-            if (process.platform !== 'darwin') app.quit()
-        })
-
-        // Silently auto update
-        if (app.isPackaged) autoUpdater.checkForUpdates()
-    })
+    mainWindow.loadURL('https://www.panfu.us/play')
 }
